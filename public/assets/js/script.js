968d879635b5e0decbd97faf0442e441beee5ee4
hixvm <hixvmx@gmail.com>
2023-05-20 16:36:03 +0100

    update js file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,12 +2,10 @@ $(document).ready(function(){
     $('.output_btn').click(function(){
         var output = $(this).next();
 
-        // output.slideToggle();
-
         if (output.css("display") == "none") {
             output.show();
             $(this).text('Hide output');
-        } else {
+        } else{
             output.hide();
             $(this).text('Show output');
         }
